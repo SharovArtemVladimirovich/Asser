6ee09f684ed29373854d0ed5a5e975756db5f7a4
SharovArtemVladimirovich <sharov-artem@bk.ru>
2025-04-15 22:16:01 +0300

    Переделан QMessageBox в QDialog
    Так как QMessageBox ограничен в плане стилизовывания и не давал возможности
    номрально вставить QLabel было принято решение о замене его на QDialog - Оно
    полностью готово и находиться в своей предфинальной версии.
    Также возможно поменять стиль выводимого изображение
    imageDialog/gif/AsserQuitText.gif и imageDialog/gif/AsserQuitTxt.gif
    
    Отрисованы и стилизованы кнопки - все работает отлично (за исключением)
    возможной анимации, пока что разбираюсь

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,7 +5,7 @@
 #include <QPushButton>
 #include <QScreen>
 #include <QWindow>
-
+#include <QMovie> // Библиотека для проигрования GIFвок
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,7 +13,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    setWindowTitle("Asser");
+    setWindowTitle("Asser"); // Устанавливаем ттитульник сверху
 
     connect(ui -> ExitButton, &QPushButton::clicked, this, &MainWindow::ExitButton_clicked);//СОЕДИНЕНИЕ кнопки ВЫХОД
     connect(ui -> AuthorButton, &QPushButton::clicked, this, &MainWindow::AuthorButton_clicked); //Соединение кнопки АВТОРЫ
@@ -58,59 +58,102 @@ void MainWindow::AuthorButton_clicked() // о Рабах системы MS DOS
 {
     Authors *authorsWindow = new Authors(); // Создаем окно, где родичь - МеинВиндоу
     authorsWindow -> setAttribute(Qt::WA_DeleteOnClose); //АвТоМаТиЧеСкОе УдАлЕнИе при закрытии
-    authorsWindow->setWindowModality(Qt::ApplicationModal);
+    authorsWindow->setWindowModality(Qt::ApplicationModal); // Делаем окно модальным
     authorsWindow -> show(); //Показ окна с авторами
 }
 
 
 void MainWindow::ExitButton_clicked() // Выход (Можно и в окно)
 {
-    //Инициализация QMessageBox
-    QMessageBox msgBox(this); //прописываем создание Массажной коробки
-    msgBox.setWindowTitle("Выйти в окно"); // Для массажной коробки установим заголовок
-    msgBox.setText("Ты точно уверен в этом?"); //вывод текста в массажную коробку
+    class CustomDialog : public QDialog { // Обьявляем класс для диалогового окна (Замена --АСС-ажной коробке)
 
-    //Обьявляем кнопки (кнопахи, кноПОЧКИ, батоны, баттоны)
-    QPushButton *noButton = msgBox.addButton("Нет, простите", QMessageBox::ActionRole); // аналогично задаем кнопку НЕТ
-    QPushButton *yesButton = msgBox.addButton("Да, отчисляйте", QMessageBox::ActionRole); //задаем тама кнопку ДА
-    //Бонусом зафигачил текст сразу в обьявление кноПАХи
+    public:
+        CustomDialog(QWidget *parent = nullptr) : QDialog(parent) { // Последние настрйоки класса (Че тут к чему мне еще надо разобраться, но пока что да - так)
 
-    noButton -> setObjectName("noButton");
-    yesButton -> setObjectName("yesButton");
+            QVBoxLayout *layoutV = new QVBoxLayout(this); // Обьявляем Лейаут для красотулечки ( V - значит ПОБЕДААААА ZZZZZ OOOOOO VVVVVV (verticale))
+            QHBoxLayout *layoutH = new QHBoxLayout(this); // также объявляем, только теперь с H (вертолетная площадка)
 
-    //устанавливаем Стиль для нашей Массажной коробки
-    msgBox.setStyleSheet("QMessageBox {"
-                         "      background-color: rgb(0,0,0);"
-                         "      color: white;"
-                         "      min-width: 530px;"
-                         "      min-height: 350px;"
-                         "}"
+            QLabel *labell = new QLabel(this); // Создаем лейбл (Ради которого я и переписывал весь msgBox в QDialog)
+            labell -> setAlignment(Qt::AlignCenter); // Респределям его по центру
 
-                         "QPushButton {"
-                         "      min-width: 250px;"
-                         "      min-height: 50px;"
-                         "      padding: 5px;"
-                         "}"
+            //А вот и QMovie (Работает пизже и легче чем звук - я ебал )
+            QMovie *movie = new QMovie(":/imagesDialog/gif/AsserQuitTxt.gif"); // Обьявлем под все добро переменную и суем в нее GIF из ресурсов
+            labell -> setMovie(movie); // Пихаем наш Виджет QMovie с муви в лейбл
+            movie -> start(); // Не забываем запустить
 
-                         "QPushButton#noButton:hover{"
-                         "      background-color: lightgreen;"
-                         "      color: black;"
-                         "      font: 16pt;"
-                         "}"
+            QPushButton *noButton = new QPushButton(this); // Обьявляем кнопку НЕЕЕЕЕЕЕЕЕЕЕЕЕТ
+            QPushButton *yesButton = new QPushButton(this);// ДААААААААААААААААААААААААААААААА
 
-                         "QPushButton#yesButton:hover{"
-                         "      background-color: red;"
-                         "      font: 16pt;"
-                         "}"
+            //Делаем из всего этого добра обьекты чтоб работать с ними в Стильном Листе
+            noButton -> setObjectName("noButton");
+            yesButton -> setObjectName("yesButton");
+            labell -> setObjectName("labell");
 
+            // Работа с Лейаутами, чтобы все ровненько, красивенько, аккуратненько (Ахуеть, я их понял и познал - преисполнился в виджетах)
+            layoutV->addWidget(labell);  // в Вертикальный кидаем лейбл
+            layoutH->addWidget(noButton); // В горизонтальный пихаем две кнопки
+            layoutH->addWidget(yesButton);
+            layoutV->addLayout(layoutH); // Суем горизонтальный в вертикальный
 
-                         );
+            connect(noButton, &QPushButton::clicked, this, &QDialog::close); // Конектим кнопку на закрытие диалогового окна
+            connect(yesButton, &QPushButton::clicked, this, &QApplication::quit); // На закрытие приложухи
+        }
+    };
 
-    msgBox.exec(); //Че то типо запуска нашего Массажного Коробка
-    if (msgBox.clickedButton() == yesButton) { //проверка на то, нажата ли кнопка ДА в массажном боксе (обращение к классу msgBox свойству clickedButton() )
-        QApplication::quit(); // Закрываем ВСЁ ПРИЛОЖЕНИЕ (Ебучий костыль исправлен)
-    }
+    CustomDialog platon; // ДИАЛОГИ ТЕТ-А-ТЕТ ( Аж Мананникову вспомнил - снова заплакал )
+    platon.setWindowTitle("Выйти в окно"); // Для Диалога установим заголовок
 
+    platon.setStyleSheet( // Ну и погнали описывать их в Стильном листе
+                        "QDialog {" // Для самого диалога
+                        "      background-color: rgb(0,0,0);" // ставим цвет фона НИГЕР (Осуждаю)
+                        "      color: white;" // цвет шрифта - господин фона
+                        "      min-width: 530px;" // ограничиваем минимум по ширине
+                        "      min-height: 230px;" // по высоте также
+                        "}"
+
+                        "QLabel#labell{" // Теперь описываем наш лейбл
+                        "      max-width: 500px;" // теперь ограничиваем максимальные размеры (по ширине)
+                        "      max-height: 40px;" // по высоте
+                        "}"
+
+                        // Общие настройки для кнопки
+                        "QPushButton {"
+                        "      max-width: 250px;" // Аналогичное ограничение
+                        "      max-height: 50px;"
+                        "      padding: 10px;" // а вот тут интересно: padding - это расстояние, которое обьект выделит себе вокруг и которую занять не смогут
+                                               // Что то типо зоны вокруг обьект, не путать с margin - отступы внутри самого обьекта от кроев до содержания
+                        "      border: none;"  // отключаем мерзкую обводку
+                        "      outline: none;" // откулючаем мерзкую подсветку
+                        "}"
+
+                        // Задание картинок для НЕЕЕЕЕЕЕЕЕТ
+                        "QPushButton#noButton{" // Ну тут в целом все по классике - почитайте ниже для goFullScreenButton_toggled, там я подробно расписал
+                        "      image: url(:/imagesDialog/images/Asser_PB_quitBNo.png);"
+                        "}"
+
+                        "QPushButton#noButton:hover{"
+                        "      image: url(:/imagesDialog/images/Asser_PB_quitBNo_hover.png);"
+                        "}"
+
+                        "QPushButton#noButton:pressed{"
+                        "      image: url(:/imagesDialog/images/Asser_PB_quitBNo_push.png);"
+                        "}"
+
+                        // Задание картинок для ДААААААААААА
+                        "QPushButton#yesButton{"
+                        "      image: url(:/imagesDialog/images/Asser_PB_quitBYes.png);"
+                        "}"
+
+                        "QPushButton#yesButton:hover{"
+                        "      image: url(:/imagesDialog/images/Asser_PB_quitBYes_hover.png);"
+                        "}"
+
+                        "QPushButton#yesButton:pressed{"
+                        "      image: url(:/imagesDialog/images/Asser_PB_quitBYes_push.png);"
+                        "}"
+
+                        );
+    platon.exec(); // Ну и конечно же запускаем наше диалоговое окно и радуемся жизни!
 }
 
 
@@ -136,7 +179,7 @@ void MainWindow::goFullScreenButton_toggled(bool checked) { //Прописыва
 
             "#goFullScreenButton:hover {" // о, а теперь прописываем СТИЛЬНЫЙ ЛИСТ для состояния, когда мы навелись мышкой на кнопку
             "   image: url(:/imagesMainMenu/images/Asser_PB_goFullScreen2_hover.png);" // вот да - другая картинка
-            "   margin-bottom: -10px;" // а вот и само смещение
+            "   margin-bottom: -10px;" // а вот и само "смещение" (просто убираем внутренний отступ кнопки и делаем эффект смещения)
             "}"
 
             "#goFullScreenButton:pressed {" // ну и наконец - состояние нажатой кнопахи
